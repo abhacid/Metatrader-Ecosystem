f83885a90388f73a7dbea2c714169721a1c12781
abhacid <ab.hacid@hotmail.fr>
2015-07-20 01:07:45 +0200

    modifications

---
+++ Indicators/HighLow/DebugHost/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace NQuotes.DebugHost
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            NQuotes.DebugHost.Server.Start(args);
+        }
+    }
+}
